dba7f962a705e38da5956d0b500b7be31a9154aa
Gabriel <Gabrielpedrofc@gmail.com>
2025-03-20 21:07:06 -0300

    ajuste

--- src/views/chatView.vue
+++ src/views/chatView.vue
@@ -4,21 +4,28 @@
 </script>
 
 <template>
-  <div class="container mt-4">
+  <div class="container my-4">
     <h4>O acesso do chamado é:</h4>
-    <div class="border rounded p-3 overflow-auto" style="height: 60vh">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-      <p>Mais conteúdo...</p>
-    </div>
-  </div>
-  <div class="container border border-subtle p-1 mt-5 rounded">
-    <div class="row">
-      <div class="col-md-10">
-        <input type="text" class="form-control" placeholder="digite a mensagem...">
+    <div class="row my-2 mt-5">
+      <div class="col-md-12">
+        <div class="border rounded overflow-auto" style="height: 60vh">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          <p>Mais conteúdo...</p>
+        </div>
       </div>
-      <div class="col-md-2">
-        <button class="btn btn-primary mx-1">Enviar</button>
-        <button class="btn btn-secondary mx-1">Anexo</button>
+    </div>
+
+    <div class="row my-5 align-items-center">
+      <div class="col-md-10">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="digite a mensagem..."
+        />
+      </div>
+      <div class="col-md-2 d-flex gap-2">
+        <button class="btn btn-primary flex-fill">Enviar</button>
+        <button class="btn btn-secondary flex-fill">Anexo</button>
       </div>
     </div>
   </div>
