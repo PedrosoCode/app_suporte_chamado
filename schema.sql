32c1e0fe5ed588438b231eff588e9b9d9e095a14
Gabriel <Gabrielpedrofc@gmail.com>
2025-03-21 22:21:45 -0300

    proc gerar chave sessão

--- schema.sql
+++ schema.sql
@@ -189,6 +189,37 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'suporte_database'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `sp_suporte_init_sessao` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_suporte_init_sessao`()
+BEGIN
+    DECLARE chave VARCHAR(6);
+    DECLARE caracteres VARCHAR(62) DEFAULT 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    
+    SET chave = CONCAT(
+        SUBSTRING(caracteres, 1 + FLOOR(RAND() * 62), 1),
+        SUBSTRING(caracteres, 1 + FLOOR(RAND() * 62), 1),
+        SUBSTRING(caracteres, 1 + FLOOR(RAND() * 62), 1),
+        SUBSTRING(caracteres, 1 + FLOOR(RAND() * 62), 1),
+        SUBSTRING(caracteres, 1 + FLOOR(RAND() * 62), 1),
+        SUBSTRING(caracteres, 1 + FLOOR(RAND() * 62), 1)
+    );
+
+    SELECT chave;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -199,4 +230,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-20 22:11:50
+-- Dump completed on 2025-03-21 22:20:29
